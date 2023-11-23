5750964236699ddb1c84757de30a253c783b788c
ahmedazim7804 <ahmedazim7804@gmail.com>
2023-11-23 21:07:57 +0530

    init repo

---
+++ android/app/src/main/kotlin/com/example/traktstats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.traktstats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:traktstats/screens/homescreen.dart';
+
+void main() {
+  runApp(const Homescreen());
+}
